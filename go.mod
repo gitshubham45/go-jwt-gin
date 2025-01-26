9035dbe16cbf26307059168f2d257bef615ea68a
gitshubham45 <shubham16290@gmail.com>
2025-01-26 22:27:08 +0530

    userControllers added

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module goJwt
 go 1.16
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.10.0
 	github.com/go-playground/validator/v10 v10.20.0
+	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/joho/godotenv v1.5.1
 	go.mongodb.org/mongo-driver v1.17.2
 	golang.org/x/crypto v0.26.0 // indirect
--- models/userModel.go
+++ models/userModel.go
@@ -7,7 +7,7 @@ import (
 
 type User struct {
 	ID            primitive.ObjectID `bson:"_id`
-	First_name    *string            `json:"first_name" validate:"required min=2 max=100"`
+	First_name    *string            `json:"first_name" validate:"required min=2 max=100"` // *string can have null value
 	Last_name     *string            `json:"last_name" validate:"required min=2 max=100"`
 	Password      *string            `json:"password" validate:"required min=6"`
 	Email         *string            `json:"email" validate:"email required"`
@@ -17,5 +17,5 @@ type User struct {
 	Refresh_token *string            `json:"refresh_token"`
 	Created_at    time.Time          `json:"created_at"`
 	Updated_at    time.Time          `json:"updated_at"`
-	User_id       string             `json:"user_id"`
+	User_id       string             `json:"user_id"` // it will have always value even empty is possible but not null
 }
